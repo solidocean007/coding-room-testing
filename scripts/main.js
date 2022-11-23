5de084c220f1e369ed847c7e825de0993c27f1c6
solidocean007 <clintonwilliams007@gmail.com>
2022-11-22 19:22:54 -0500

    Create scripts folder
    *Add main.js file and worked on variables.

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var dateOfBirth = '3/10/1978';
+var age = '44';
+var isOrganDonor = 'yes';
+var streetAddress = '7013 trout creek rd';
+var city = 'fayetteville';
+var state = 'NC';
+var zippostalcode = '28304';
+var idNumber = '5457099';
+var etc = 'Brown';
+
+var driverMessage = 'Hi, my name is Clinton Williams and I was born on' + ' ' + dateOfBirth + ' ' + 'and I live at' + ' ' + streetAddress + ' '
+  + city + ' ' + state + ' ' + zippostalcode  + '.  ' + 'My id number is:' + idNumber + '.  ';
+
+console.log(driverMessage);
