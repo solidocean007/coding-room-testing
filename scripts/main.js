f218247680c375c379deb6bdfd31413861759069
solidocean007 <clintonwilliams007@gmail.com>
2022-11-23 16:09:32 -0500

    Complete the Exercise: Booleans & conditional logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,9 @@
-var beer = 14.99;
-var chicken = 10.55;
-var eggs = 12.99;
-var bananas = 5.99;
-var bread = 4.99;
-var steak = 28.99;
+var currentLightColor = 'Yellow';
 
-var totalCost = beer + chicken + eggs + bananas + bread + steak;
-
-console.log('Total Cost: ' + totalCost.toFixed(2));
-
-var coupon25PercentOff = .25;
-
-var priceAfterDiscount = totalCost - (totalCost * coupon25PercentOff);
-
-var taxes = .0775 * priceAfterDiscount;
-
-console.log('Price after coupon: ' + priceAfterDiscount);
-
-console.log('Tax: ' + taxes);
-
-var finalPrice = priceAfterDiscount + taxes;
-
-console.log(finalPrice.toFixed(2));
+if (currentLightColor == 'Green') {
+  console.log('The bottom light is ' + currentLightColor + '.  ');
+} else if (currentLightColor == 'Yellow') {
+  console.log('The middle light is ' + currentLightColor + '.  ');
+} else if (currentLightColor == 'Red') {
+    console.log ('The top light is ' + currentLightColor + '.  ');
+  }
