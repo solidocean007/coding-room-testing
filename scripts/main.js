421ef381c82b58d79bd4d40a502ce5005856a61a
solidocean007 <clintonwilliams007@gmail.com>
2022-11-22 19:42:18 -0500

    work with javascript numbers

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,10 @@
-var dateOfBirth = '3/10/1978';
-var age = '44';
-var isOrganDonor = 'yes';
-var streetAddress = '7013 trout creek rd';
-var city = 'fayetteville';
-var state = 'NC';
-var zippostalcode = '28304';
-var idNumber = '5457099';
-var etc = 'Brown';
+var totalUser = 42;
+var purchase = 46.99;
 
-var driverMessage = 'Hi, my name is Clinton Williams and I was born on' + ' ' + dateOfBirth + ' ' + 'and I live at' + ' ' + streetAddress + ' '
-  + city + ' ' + state + ' ' + zippostalcode  + '.  ' + 'My id number is:' + idNumber + '.  ';
+var sciNot = 123e5;
 
-console.log(driverMessage);
+var numVal = 5 * 6;
+
+
+console.log(sciNot);
+console.log(numVal);
