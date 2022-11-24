8281f1d249ab95f0e920e76de6a7cd016bc61662
solidocean007 <clintonwilliams007@gmail.com>
2022-11-23 21:38:02 -0500

    FizzBuzz

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,14 @@
-var tvShows = [
-  'The Walking Dead', 
-  'Breaking Bad', 
-  'The Office'
-];
 
-var completedItems = [];
-
-var toDos = [
-  { title: 'Item 1', completed: true},
-  { title: 'Item 2', completed: false},
-  { title: 'Item 3', completed: false},
-  { title: 'Item 4', completed: true},
-]
-
-for (var x = 0; x < toDos.length; x++) {
-  if (toDos[x].completed) {
-    completedItems.push(toDos[x]);
+var x = 1;
+  while (x < 100) {
+  if (x % 3 == 0 && x % 5 == 0) {
+    console.log('FizzBuzz');
   }
-}
-
-var bottles = 99;
-while (bottles > 0) {
-  if (bottles != 1) {
-    console.log(bottles + ' bottles of stuff on the wall');
-  } else {
-    console.log(bottles + ' bottle of stuff on the wall');
+  else if (x % 3 == 0) {
+    console.log('Fizz');
+  } else if (x % 5 == 0) {
+    console.log('Buzz');
   }
-  bottles -= 1;
+  else {console.log(x)};
+  x++;
 }
-
-
-
-// console.log(toDos);
-// console.log(completedItems);
-
-// for (var i = 0; i < tvShows.length; i++) {
-//   console.log(tvShows[i]);
-// }
